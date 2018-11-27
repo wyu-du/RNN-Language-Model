fa659c088282a3b25b7d9933e67bd5c494611fa5
think <310445479@qq.com>
2018-11-27 13:41:29 -0500

    fix bug in strip

--- perplexity.py
+++ perplexity.py
@@ -8,8 +8,7 @@ Created on Sun Nov 25 11:23:11 2018
 import torch
 import numpy as np
 import os
-from simple_rnnlm import SimpleRNN, get_sent_id, get_sent_tensor
-import argparse
+from stackedlstm_rnnlm import StackedRNN, get_sent_id, get_sent_tensor
 
 
 USE_CUDA = torch.cuda.is_available()
@@ -33,13 +32,13 @@ def perplexity(corpus_name, preds, targets):
 def load_model(checkpoint_name, hidden_size):
     # load model weights from checkpoints
     load_filename = os.path.join('checkpoints', '{}.tar'.format(checkpoint_name))
-    checkpoint = torch.load(load_filename)
+    checkpoint = torch.load(load_filename, map_location='cpu')
     rnn_sd = checkpoint['rnn']
     vocab_dict = checkpoint['vocab_dict']
     
     # build model
     vocab_size = len(vocab_dict.keys())
-    rnn = SimpleRNN(vocab_size, hidden_size)
+    rnn = StackedRNN(vocab_size, hidden_size, 2)
     rnn.load_state_dict(rnn_sd)
     rnn = rnn.to(device)
     
@@ -67,6 +66,46 @@ def get_perplexity_number(file_name, rnn, vocab_dict):
     return p
 
 
+def get_logprob(file_name, output_fname, rnn, vocab_dict):
+    rnn.eval()
+    
+    # load data
+    sents_id = get_sent_id(file_name, vocab_dict)
+    
+    outs = open(output_fname, 'w', encoding='utf8')
+    for i in range(len(sents_id)):
+        input_variables = get_sent_tensor(sents_id[i])
+        # create initial hidden_state
+        hidden_state = (torch.zeros(1, 1, rnn.hidden_size).to(device), 
+                        torch.zeros(1, 1, rnn.hidden_size).to(device))
+        pred_sent = ''
+        # forward batch of tokens through rnn one time step at a time
+        for t in range(input_variables.size()[0]-1):
+            output, hidden_state = rnn(input_variables[t], hidden_state)
+            prob = torch.gather(output, 1, input_variables[t+1].view(1,-1))
+            prob = prob.cpu().detach().numpy()[0]
+            pred_sent += str(prob[0])+'\t'
+        outs.write(pred_sent+'\n')
+    outs.close()
+
+
+def compute_perplexity(corpus_name):
+    with open(corpus_name, 'r', encoding='utf8') as f:
+        lines = f.read().strip().split('\n')
+    print('Read {!s} sentences.'.format(len(lines)))
+    n_total = 0.
+    p_total = 0.
+    for line in lines:
+        tokens = line.strip().split('\t')
+        n_total += len(tokens)+1
+        sent_p = 0.
+        for i in range(len(tokens)):
+            sent_p += float(tokens[i].strip())
+        p_total += sent_p
+    avg = p_total/n_total
+    out = np.exp(-avg)
+    return out
+
 
 def get_tst_logprob(file_name, output_fname, rnn, vocab_dict):
     rnn.eval()
@@ -91,61 +130,15 @@ def get_tst_logprob(file_name, output_fname, rnn, vocab_dict):
             pred_sent += new_dict[word_idx]+'\t'+str(prob[0])+' '
         outs.write(pred_sent+'\n')
     outs.close()
-    
-    
-def get_logprob(file_name, output_fname, rnn, vocab_dict):
-    rnn.eval()
-    
-    # load data
-    sents_id = get_sent_id(file_name, vocab_dict)
-    
-    outs = open(output_fname, 'w', encoding='utf8')
-    for i in range(len(sents_id)):
-        input_variables = get_sent_tensor(sents_id[i])
-        # create initial hidden_state
-        hidden_state = (torch.zeros(2, 1, rnn.hidden_size).to(device), 
-                        torch.zeros(2, 1, rnn.hidden_size).to(device))
-        pred_sent = ''
-        # forward batch of tokens through rnn one time step at a time
-        for t in range(input_variables.size()[0]-1):
-            output, hidden_state = rnn(input_variables[t], hidden_state)
-            prob = torch.gather(output, 1, input_variables[t+1].view(1,-1))
-            prob = prob.cpu().detach().numpy()[0]
-            pred_sent += str(prob[0])+'\t'
-        outs.write(pred_sent+'\n')
-    outs.close()
-
-
-def compute_perplexity(corpus_name):
-    with open(corpus_name, 'r', encoding='utf8') as f:
-        lines = f.read().strip().split('\n')
-    print('Read {!s} sentences.'.format(len(lines)))
-    n_total = 0.
-    p_total = 0.
-    for line in lines:
-        tokens = line.split('\t')
-        n_total += len(tokens)+1
-        sent_p = 0.
-        for i in range(len(tokens)):
-            sent_p += float(tokens[i])
-        p_total += sent_p
-    avg = p_total/n_total
-    out = np.exp(-avg)
-    return out
-
 
 
 if __name__=='__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--name', default='10000_checkpoint_simple')
-    args = parser.parse_args()
+    rnn, vocab_dict = load_model('10000_checkpoint_stack_2', 32)
     
-    rnn, vocab_dict = load_model(args.name, 32)
-
-    get_logprob('trn-wiki.txt', 'trn-logprob.txt', rnn, vocab_dict)
-    get_logprob('dev-wiki.txt', 'dev-logprob.txt', rnn, vocab_dict)
-    train_perplexity = compute_perplexity('trn-logprob.txt')
+    get_logprob('trn-wiki.txt', 'trn-logprob-stack2.txt', rnn, vocab_dict)
+    get_logprob('dev-wiki.txt', 'dev-logprob-stack2.txt', rnn, vocab_dict)
+    train_perplexity = compute_perplexity('trn-logprob-stack2.txt')
     print('Perplexity on training set:',train_perplexity)
-    dev_perplexity = compute_perplexity('dev-logprob.txt')
+    dev_perplexity = compute_perplexity('dev-logprob-stack2.txt')
     print('Perplexity on development set:',dev_perplexity)
     
